7510c4d77a85ec2bab63dd7998fd8d441a969b46
Nicolas Butarbutar <nicolaszuliaaan@gmail.com>
2024-12-04 16:12:47 +0700

    Kami sudah menyelesaikan tugas.

--- X02.java
+++ X02.java
@@ -1,2 +1,54 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24038 - NICOLAS J GRACE BUTARBUTAR
+// 12S24026 - GRISELDA TABITHA NATHANIA HUTAHAEAN
+
+import java.util.*;
+import java.lang.Math;
+
+public class X02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nama = new String[4];
+        int i;
+
+        i = 0;
+        initialized(nama, i);
+        var_input(nama);
+        output(nama);
+    }
+    
+    public static void initialized(String[] nama, int i) {
+        String initialized;
+
+        for (i = 0; i <= 3; i++) {
+            nama[i] = "";
+        }
+    }
+    
+    public static void var_input(String[] nama) {
+        String var_input;
+        int i;
+
+        for (i = 0; i <= 3; i++) {
+            nama[i] = input.nextLine();
+            if (nama[i].equals("---")) {
+                i = 9;
+            }
+        }
+    }
+    
+    public static void output(String[] nama) {
+        String output;
+        int i;
+
+        for (i = 0; i <= 3; i++) {
+            if (nama[i].equals("---")) {
+                i = 72;
+            } else {
+                System.out.println(nama[i]);
+            }
+        }
+    }
+}
+
+
